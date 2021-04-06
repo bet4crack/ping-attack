5e8e254262591cc7dd7c28492752a7ea2d9be1f0
bet4crack <79579233+bet4crack@users.noreply.github.com>
2021-04-06 15:27:30 +0300

    Add files via upload

---
+++ ping-attack.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+os.system("clear")
+
+def logo():
+	print("    +=========================================+")
+	print("    |..........   PING-ATTACK      ...........|")
+	print("    +-----------------------------------------+")
+	print("    |            #Author: BET4 CODER          |")
+	print("    |       https://t.me/bet4crack            |")
+	print("    +=========================================+")
+	print("    |..........   PING-ATTACK      ...........|")
+	print("    +-----------------------------------------+")
+	print("                                               ")
+
+print("                                          ")
+print("    Author : BET4 CODER                   ")
+print("                                          ")
+print("    About  : tool for ddos attacking      ")
+print("                                          ")
+print("    About  : ba vpn bakary bina!          ")
+print("                                          ")
+ip = input("\u001b[31mTARGET IP  [>] ")
+print("                                          ")
+logo()
+os.system("\u001b[33mping  {}  ".format(ip))
+
