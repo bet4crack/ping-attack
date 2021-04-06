7ec62a410b996d9f5384cae274d326c3bc111768
bet4crack <79579233+bet4crack@users.noreply.github.com>
2021-04-06 15:26:59 +0300

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,37 @@
+#---------------------------------
+# CRATED BY : TANO
+# KURDISH   : 1.1.1
+#--------------------------------
+import os
+import sys
+
+os.system("clear")
+os.system("pkg install toilet")
+os.system("clear")
+os.system("toilet PING ATTACK")
+print("                                            ")
+
+print("DDos Attack                                 ")
+
+print("                                            ")
+
+print("Author   : Tano Awat                        ")
+
+print("                                            ")
+
+print("Facebook : https://www.facebook.com/KURD404 ")
+
+print("                                            ")
+
+print("zanyary  : ba vpn bakary bina          ")
+
+print("                                       ")
+
+ip = input("\u001b[31mTARGET IP  [>] ")
+print("                    ")
+lp = input("\033[0;35mSet Amount [>] ")
+print("                    ")
+kp = input("\u001b[36mSet Count  [>] ")
+print("                    ")
+os.system("\u001b[33mping  {}  ".format(ip,lp,kp))
+
